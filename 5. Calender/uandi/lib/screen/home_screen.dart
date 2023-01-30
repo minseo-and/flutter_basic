10bb71fa05f499cbb5fea3c6752534ec2b833322
minseo <msp05070478@gmail.com>
2023-01-30 17:23:41 +0900

    CupertinoDatePicker 구현 완료

--- 5. Calender/uandi/lib/screen/home_screen.dart
+++ 5. Calender/uandi/lib/screen/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -24,11 +25,24 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 }
 
-class _TopPart extends StatelessWidget {
+class _TopPart extends StatefulWidget {
   const _TopPart({Key? key}) : super(key: key);
 
   @override
+  State<_TopPart> createState() => _TopPartState();
+}
+
+class _TopPartState extends State<_TopPart> {
+  DateTime selectedDate = DateTime(
+      DateTime.now().year,
+      DateTime.now().month,
+      DateTime.now().day
+  );
+
+  @override
   Widget build(BuildContext context) {
+    final now = DateTime.now();
+
     return Expanded(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -47,9 +61,9 @@ class _TopPart extends StatelessWidget {
                     fontFamily: 'sunflower',
                     fontSize: 30.0),
               ),
-              const Text(
-                '2021.12.27',
-                style: TextStyle(
+              Text(
+                '${selectedDate.year}.${selectedDate.month}.${selectedDate.day}',
+                style: const TextStyle(
                     color: Colors.white,
                     fontFamily: 'sunflower',
                     fontSize: 20.0),
@@ -59,7 +73,7 @@ class _TopPart extends StatelessWidget {
           IconButton(
               iconSize: 60.0,
               onPressed: () {
-                showDialog(
+                showCupertinoDialog(
                     context: context,
                     barrierDismissible: true,
                     builder: (BuildContext context) {
@@ -68,7 +82,20 @@ class _TopPart extends StatelessWidget {
                         child: Container(
                           color: Colors.white,
                           height: 300.0,
-                          child: ,
+                          child: CupertinoDatePicker(
+                            mode: CupertinoDatePickerMode.date,
+                            initialDateTime: selectedDate,
+                            maximumDate: DateTime(
+                              now.year,
+                              now.month,
+                              now.day
+                            ),
+                            onDateTimeChanged: (DateTime date) {
+                              setState(() {
+                                selectedDate = date;
+                              });
+                            },
+                          ),
                         ),
                       );
                     });
@@ -77,9 +104,13 @@ class _TopPart extends StatelessWidget {
                 Icons.favorite,
                 color: Colors.red,
               )),
-          const Text(
-            'D+1',
-            style: TextStyle(
+          Text(
+            'D+${DateTime(
+              now.year,
+              now.month,
+              now.day
+            ).difference(selectedDate).inDays + 1}',
+            style: const TextStyle(
                 color: Colors.white,
                 fontFamily: 'sunflower',
                 fontSize: 50.0,
