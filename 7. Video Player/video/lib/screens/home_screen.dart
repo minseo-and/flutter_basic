a6a9e94e5b74e83587cd8fb40a1dc97fcff3d168
Minseo <msp05070478@gmail.com>
2023-02-01 13:55:28 +0900

    UI 완성

--- 7. Video Player/video/lib/screens/home_screen.dart
+++ 7. Video Player/video/lib/screens/home_screen.dart
@@ -7,12 +7,72 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+
+
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Home Screen'),
+    return Scaffold(
+      body: Container(
+       decoration: getBoxDecoration(),
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            _Logo(),
+            SizedBox(height: 30.0,),
+            _AppName(),
+          ],
+        ),
       ),
     );
   }
+  BoxDecoration getBoxDecoration() {
+    return const BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [
+            Color(0xff2a3a7c),
+            Color(0xff000118)
+          ],
+        )
+    );
+  }
 }
+
+class _Logo extends StatelessWidget {
+  const _Logo({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset('assets/images/logo.png');
+  }
+}
+
+class _AppName extends StatelessWidget {
+  const _AppName({Key? key}) : super(key: key);
+
+  final textStyle = const TextStyle(
+      color: Colors.white,
+      fontSize: 30.0,
+      fontWeight: FontWeight.w300
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text('VIDEO',
+            style: textStyle),
+        Text('PLAYER',
+            style: textStyle.copyWith(
+                fontWeight: FontWeight.w700
+            ))
+      ],
+    );
+  }
+}
+
+
